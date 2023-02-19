4033a74e5104fe93047b3dc4d21bdc68b1e13985
Judalm Park (dami) <wnekfa1004@naver.com>
2023-02-19 23:53:47 +0900

    Repository 테스트 환경 세팅 완료

--- src/main/java/site/metacoding/junitproject/domain/BookRepository.java
+++ src/main/java/site/metacoding/junitproject/domain/BookRepository.java
@@ -1,4 +1,9 @@
 package site.metacoding.junitproject.domain;
 
-public interface BookRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository // JpaRepository 상속 시 생략가능
+public interface BookRepository extends JpaRepository<Book,Long> {
+
 }
