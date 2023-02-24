14e24f5f052719838f60418d7121a10133226353
Judalm Park (dami) <wnekfa1004@naver.com>
2023-02-25 01:28:47 +0900

    서비스레이어 테스트 - 메서드 참조 코드 리팩토링

--- src/main/java/site/metacoding/junitproject/web/dto/BookRespDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/BookRespDto.java
@@ -1,22 +1,20 @@
 package site.metacoding.junitproject.web.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import site.metacoding.junitproject.domain.Book;
 
 @Getter
 @NoArgsConstructor
 public class BookRespDto {
-
     private Long id;
     private String title;
     private String author;
 
-    public BookRespDto toDto(Book bookPS){
-        this.id = bookPS.getId();
-        this.title = bookPS.getTitle();
-        this.author = bookPS.getAuthor();
-        return this;
+    @Builder
+    public BookRespDto(Long id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
     }
-
 }
