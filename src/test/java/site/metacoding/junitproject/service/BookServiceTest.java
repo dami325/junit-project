2e42dd8b377c2a90ef5a8880295d7febfc32f612
Judalm Park (dami) <wnekfa1004@naver.com>
2023-02-24 10:02:32 +0900

    서비스 레이어 테스트 - 책등록하기 stub, mock, assertJ 완료

--- src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
+++ src/test/java/site/metacoding/junitproject/service/BookServiceTest.java
@@ -1,21 +1,33 @@
 package site.metacoding.junitproject.service;
 
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import site.metacoding.junitproject.domain.BookRepository;
-import site.metacoding.junitproject.util.MailSenderStub;
+import site.metacoding.junitproject.util.MailSender;
 import site.metacoding.junitproject.web.dto.BookRespDto;
 import site.metacoding.junitproject.web.dto.BookSaveReqDto;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@DataJpaTest
+@ExtendWith(MockitoExtension.class)
 public class BookServiceTest {
 
-    @Autowired // DI
+    @InjectMocks
+    private BookService bookService;
+
+    @Mock
     private BookRepository bookRepository;
+
+    @Mock
+    private MailSender mailSender;
+
 
     // 문제점 -> 서비스만 테스트하고 싶은데, 레포지토리 레이어가 함께 테스트 된다는점
     @Test
@@ -24,18 +36,20 @@ public class BookServiceTest {
         BookSaveReqDto dto = new BookSaveReqDto();
         dto.setTitle("junit강의");
         dto.setAuthor("메타코딩");
-        
-        // stub 가짜를 만들어내는 작업
-        MailSenderStub mailSenderStub = new MailSenderStub();
-        // 가짜로 bookRepository 만들기!!
-        
+
+        // stub 가짜를 만들어내는 작업 ( 행동 정의, 가설 )
+        when(bookRepository.save(any())).thenReturn(dto.toEntity());
+        when(mailSender.send()).thenReturn(true);
+
         // when
-        BookService bookService = new BookService(bookRepository, mailSenderStub);
         BookRespDto bookRespDto = bookService.책등록하기(dto);
 
         //then
-        assertEquals(dto.getTitle(), bookRespDto.getTitle());
-        assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+//        assertEquals("junit강의", bookRespDto.getTitle());
+//        assertEquals(dto.getAuthor(), bookRespDto.getAuthor());
+
+        assertThat(dto.getTitle()).isEqualTo(bookRespDto.getTitle());
+        assertThat(dto.getAuthor()).isEqualTo(bookRespDto.getAuthor());
 
 
     }
