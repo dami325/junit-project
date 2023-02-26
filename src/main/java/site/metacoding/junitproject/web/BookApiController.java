92c0b6286c1858ade2edd2e104de14cc8bedc4cf
Judalm Park (dami) <wnekfa1004@naver.com>
2023-02-26 22:41:51 +0900

    컨트롤러 레이어 본코드 - 책저장하기

--- src/main/java/site/metacoding/junitproject/web/BookApiController.java
+++ src/main/java/site/metacoding/junitproject/web/BookApiController.java
@@ -1,7 +1,57 @@
 package site.metacoding.junitproject.web;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import site.metacoding.junitproject.service.BookService;
+import site.metacoding.junitproject.web.dto.response.BookRespDto;
+import site.metacoding.junitproject.web.dto.request.BookSaveReqDto;
+import site.metacoding.junitproject.web.dto.response.CMRespDto;
 
+@RequiredArgsConstructor
 @RestController
-public class BookApiController {
+public class BookApiController { // 컴포지션 = has 관계
+
+    /**
+     * final이 붙었을땐 컨트롤러 호출시 초기화가 돼야함
+     */
+    private final BookService bookService;
+
+    // 1. 책등록
+    // key = value&key = value
+    // { "key" : value, "key" : value }
+    @PostMapping("/api/v1/book")
+    public ResponseEntity<?> saveBook(@RequestBody BookSaveReqDto bookSaveReqDto) {
+        BookRespDto bookRespDto = bookService.책등록하기(bookSaveReqDto);
+        CMRespDto<?> cmRespDto = CMRespDto.builder()
+                .code(1)
+                .msg("글 저장 성공")
+                .body(bookRespDto)
+                .build();
+        return new ResponseEntity<>(cmRespDto,HttpStatus.CREATED); // 201 = insert
+    }
+
+    // 2. 책목록보기
+    public ResponseEntity<?> getBookList() {
+        return null;
+    }
+
+    // 3. 책한건보기
+    public ResponseEntity<?> getBookOne() {
+        return null;
+    }
+
+    // 4. 책삭제하기
+    public ResponseEntity<?> deleteBook() {
+        return null;
+    }
+
+    // 5. 책수정하기
+    public ResponseEntity<?> updateBook() {
+        return null;
+    }
+
 }
