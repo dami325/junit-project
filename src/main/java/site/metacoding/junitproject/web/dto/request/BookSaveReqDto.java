6211a624fe1372e22130d21aa928e71838e0c489
Judalm Park (dami) <wnekfa1004@naver.com>
2023-02-26 23:02:48 +0900

    컨트롤러 레이어 본코드 - 책저장하기 - 유효성 검사

--- src/main/java/site/metacoding/junitproject/web/dto/request/BookSaveReqDto.java
+++ src/main/java/site/metacoding/junitproject/web/dto/request/BookSaveReqDto.java
@@ -4,10 +4,22 @@ import lombok.Getter;
 import lombok.Setter;
 import site.metacoding.junitproject.domain.Book;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Setter // Controller에서 Setter가 호출되면서 Dto에 값이 채워짐.
 @Getter
 public class BookSaveReqDto {
+
+    /**
+     * @NotBlank는 NotNull + NotEmpty
+     */
+    @Size(min = 1, max = 50)
+    @NotBlank
     private String title;
+
+    @Size(min = 2, max = 20)
+    @NotBlank
     private String author;
 
     public Book toEntity() {
